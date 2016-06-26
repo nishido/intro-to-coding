d67619840e7b67f9eb4f99d5d05a2aa15b8ec691
Jason <blee.jason@gmail.com>
2016-06-26 21:13:59 +0000

    Completes chapter on variables

---
+++ variables/age.rb
@@ -0,0 +1,12 @@
+puts "How old are you?"
+
+age = gets.chomp
+
+puts "In 10 years you will be:"
+puts 10 + age.to_i
+puts "In 20 years you will be:"
+puts 20 + age.to_i
+puts "In 30 years you will be:"
+puts 30 + age.to_i
+puts "In 40 years you will be:"
+puts 40 + age.to_i
---
+++ variables/name.rb
@@ -0,0 +1,14 @@
+puts "Greetings human! What is your primary instance declaration (first name)?"
+
+first_name = gets.chomp
+
+puts "Excellent. And your secondary instance declaration (surname)?"
+
+last_name = gets.chomp
+
+puts "Very nice to meet you #{first_name} #{last_name}! I do hope you aren't murdered in your sleep by the night terrors!"
+
+
+10.times do
+	puts first_name
+end
