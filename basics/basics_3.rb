2549f7e5dbe02983b5b08ea59830c8b6c3af2279
Jason <blee.jason@gmail.com>
2016-06-26 18:39:57 +0000

    adds solutions for problems 3 and 4

---
+++ basics/basics_3.rb
@@ -0,0 +1,16 @@
+movies = {:the_matrix => "1999", :the_shawshank_redemption => "1992",
+					:star_wars => "1976", :jurrasic_park => "1993", :warrior => "2011"}
+					
+movies_array=[]					
+					
+movies_array[0] = movies[:the_matrix]
+movies_array[1] = movies[:the_shawshank_redemption]
+movies_array[2] = movies[:star_wars]
+movies_array[3] = movies[:jurrasic_park]
+movies_array[4] = movies[:warrior]
+
+puts movies_array[0]
+puts movies_array[1]
+puts movies_array[2]
+puts movies_array[3]
+puts movies_array[4]
